d44c78a71fd21fdce68e28d1312b76d108950577
Thomas PEN <thomas.pen@epitech.eu>
2023-10-18 02:26:07 +0200

    commit generated with gh-pages

--- myApp.js
+++ myApp.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 const mongo_URI = process.env.MONGO_URI;
+var ghpages = require('gh-pages');
+
+
 // to assign variables to other variables in .env, you have to use dotenv-expand
 // otherwise, if you want to compose your varaibles, you have to concatenate the final variable in js, as .env by itself can only store raw strings.
 
@@ -73,3 +76,10 @@ exports.createManyPeople = createManyPeople;
 exports.removeById = removeById;
 exports.removeManyPeople = removeManyPeople;
 exports.queryChain = queryChain;
+
+console.log("publishing with gh-pages")
+ghpages.publish('./', {
+  branch: 'main',
+  remote: 'github-origin',
+  message: 'commit generated with gh-pages'
+});
